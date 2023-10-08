4d96cbad52f0b1a3b66df1cc53bd020b1aa37a81
Abhinand Krishna AM <am.abhinandkrishna@gmail.com>
2023-10-08 11:31:50 +0530

    header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header =()=>{
+    return(
+        <div>
+           <header id="header" className="fixed-top d-flex align-items-center header-transparent">
+            <div className="container d-flex justify-content-between align-items-center">
+                
+
+            </div>
+
+           </header>
+        </div>
+    );
+}
+export default Header;
